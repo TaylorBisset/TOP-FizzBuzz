3ea67a4850468892ca900b46451a146092a8c3df
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 17:08:00 -0700

    pseudocode written

--- javascript.js
+++ javascript.js
@@ -1,2 +1,20 @@
 // Javascript.js TOP-FizzBuzz
 
+/*
+Write a program that takes a user’s input and prints the numbers 
+from one to the number the user entered. 
+However, for multiples of three print Fizz instead of the number 
+and for the multiples of five print Buzz. 
+For numbers which are multiples of both three and five print FizzBuzz.
+*/
+
+// Planning
+
+/* 
+When a user inputs a number
+Loop from 1 to the entered number
+If the current number is divisible by 3 then print "Fizz"
+If the current number is divisible by 5 then print "Buzz"
+If the current number is divisible by 3 and 5 then print "FizzBuzz"
+Otherwise print the current number
+*/
