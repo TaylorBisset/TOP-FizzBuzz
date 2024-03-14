5a2d717f3292cd04af923ff1cc3c59b4ba0bbb0e
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 17:12:04 -0700

    add Fizz to for loop

--- javascript.js
+++ javascript.js
@@ -18,3 +18,17 @@ If the current number is divisible by 5 then print "Buzz"
 If the current number is divisible by 3 and 5 then print "FizzBuzz"
 Otherwise print the current number
 */
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+for (let i = 1; i <= answer; i++) 
+{
+    if (i % 3 === 0)
+    {
+        console.log("Fizz");
+    }
+    else
+    {
+        console.log(i);
+    }
+}
