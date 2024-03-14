80a26c61eba9e7211ba44434cd253c800e690c12
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 17:13:34 -0700

    add Buzz to for loop

--- javascript.js
+++ javascript.js
@@ -27,6 +27,10 @@ for (let i = 1; i <= answer; i++)
     {
         console.log("Fizz");
     }
+    else if (i % 5 === 0)
+    {
+        console.log("Buzz");
+    }
     else
     {
         console.log(i);
